a5b301e08ea79b8865d009c666f97c399358da4c
qingshan <446472854@qq.com>
2022-08-23 19:22:35 +0800

    dev02 提交的结果

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ function LogDemo(name) {
 	return name
 }
 console.log(LogDemo("asdas"))
+let name = {
+	name: "小红",
+	age: 12,
+	sex: "男"
+}
+class People {
+	constructor(name, age, sex) {
+		this.name = name
+		this.age = age
+		this.sex = sex
+	}
+}
+
+let p1 = new People()
